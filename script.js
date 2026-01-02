b5834f8efb629b0e1fb8194cc3d15aa4f72f6786
enoch03557 <enochomolasoye@gmail.com>
2026-01-02 18:09:31 +0000

    Fix event listener issues and improve section handling

--- script.js
+++ script.js
@@ -6,20 +6,43 @@ function theme(){
 }
 
 const themeButton = document.getElementById('themeButton');
-
-themebutton.addEventListener('click', theme);
+themeButton.addEventListener('click', theme);
 
 //sections' remove buttons functionality
+const sections = document.querySelector('.section');
 
-function removeSec(){
-  section.style.display = 'none';
-  console.log('removed a button');
-}
-
-const section = document.getElementsByClassName('section')
-const button = document.querySelector('section button')
-
-section.forEach((s) =>{
-  button.addEventListener('click', removeSec);
+sections.forEach((section) =>{
+  button.addEventListener('click', => {
+    const button = document.querySelector('section button');
+    
+    section.style.display = 'none';
+    console.log('section has been removed');
+  })
 })
+
+//special buttons functionality
+
+const allBtn = document.getElementById('all')
+const activeBtn = document.getElementById('active')
+const inactiveBtn = document.getElementById('inactive')
+
+allBtn.addEventListener('click', sort('all'));
+activeBtn.addEventListener('click', sort('active'));
+inactiveBtn.addEventListener('click', sort('inactive'));
+
+
+function sort(type){
+  const section = document.querySelector('section')
+  const checkbox = document.querySelector('input[type='checkbox']');
+  const isActive = checkbox.checked;
+ if(type === 'all'){
+   section.style.display = 'grid';
+ } else if(type === 'active' && isActive){
+   section.style.display = 'grid';
+ } else if(type ==='inactive' && !isActive){
+   section.style.display = 'grid';
+ } else{
+   section.style.display = 'none';
+ }
+}
 
